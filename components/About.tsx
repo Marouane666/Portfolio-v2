fe0465cac9257537eeab0792bd008eb6092a9ce1
gero <marouanetabaa@gmail.com>
2025-05-25 12:22:28 +0100

    navbar test

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 export const About = () => {
-  const [activeTab, setActiveTab] = useState<"work" | "education">("work");
+  const [activeTab] = useState<"work" | "education">("work");
 
   const workExperience = [
     {
