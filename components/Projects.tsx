cd07befa8e3ce3d8b40935983d67d0a6341b503d
gero <marouanetabaa@gmail.com>
2025-05-28 12:46:16 +0100

    ijkdfsg fjkudsgkuj

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import MathButtonWith3DShapes from "./MathButtonWith3DShapes";
+
 
 export function Projects() {
   const projects = [
