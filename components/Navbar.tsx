788e460a8f9404b47ec183749d65dcdc07fbe0a2
gero <marouanetabaa@gmail.com>
2025-05-28 11:08:21 +0100

    dsffddfs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
               setActiveSection(null);
             } else {
               setActiveSection(sectionId);
-            }
+            } 
           }
         },
         {
